3d77d56293728e23f0cefe321f9956228771ca17
Gabriela Furst <gabifurst2@gmail.com>
2024-06-27 16:26:08 -0300

    Comparação

---
+++ comparacao.py
@@ -0,0 +1,9 @@
+primeiro_numero = input('Digite um número: ')
+segundo_numero = input('Digite outro número: ')
+
+if primeiro_numero > segundo_numero:
+    print('Primeiro número é maior que o segundo!')
+elif primeiro_numero < segundo_numero:
+    print('Segundo número é maior que o primeiro número!')
+else:
+    print('Os número digitados são iguais!')        
