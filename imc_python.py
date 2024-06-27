33ea46c4379b7ad0ad8cc3905ce63f28f8132fc9
Gabriela Furst <gabifurst2@gmail.com>
2024-06-27 16:20:17 -0300

    Exercícios Python

---
+++ imc_python.py
@@ -0,0 +1,6 @@
+nome = "Gabriela"
+altura = 1.60
+peso = 50
+imc = peso/(altura * altura)
+
+print(f'Meu nome é {nome}, eu peso {peso} quilos e minha altura é {altura}, nesse caso meu imc é {imc}.')
---
+++ variaveis_python.py
@@ -0,0 +1,17 @@
+nome = 'Tereza'
+segundo_nome = 'Cristina'
+sobrenome = 'Furst Campos'
+ano_de_nascimento = 2011
+idade = 2024 - ano_de_nascimento
+especie = 'Felina'
+pelagem = 'Longa'
+cor = 'Marrom'
+
+print('Nome:', nome)
+print('Segundo nome:', segundo_nome)
+print('Sobrenome:', sobrenome)
+print('Ano de nascimento:', ano_de_nascimento)
+print('Idade:', idade)
+print('Espécie:', especie)
+print('Pelagem:', pelagem)
+print('Cor:', cor)
