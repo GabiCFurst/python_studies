40141e0495f60f5718e452507d21677aed1c7ca8
Gabriela Furst <gabifurst2@gmail.com>
2024-07-11 17:58:26 -0300

    Atualização lista dentro de lista

--- lista_dentro_de_lista.py
+++ lista_dentro_de_lista.py
@@ -6,8 +6,10 @@ salas = [
 
 ['Rajadinha', 'Tereza', 'Limão'],
 
-['Tetê', 'Chico', 'Liz', 'Marley', 'Salusa']
+['Tetê', 'Chico', 'Liz', 'Marley', 'Salusa', (10, 20, 30, 40, 50)]
 
 ]
 
-print(salas[1][3])
+print(salas[3][3])
+print(salas[0][2])
+print(salas[3][5][1]) #Ir acessando a lista de onde quer puxar o indicie expecífico.
