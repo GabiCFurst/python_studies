3358ae71913e30cb64583ed8c288898839f9ed05
Gabriela Furst <gabifurst2@gmail.com>
2024-07-11 17:51:06 -0300

    Lista dentro de lista

---
+++ lista_dentro_de_lista.py
@@ -0,0 +1,13 @@
+salas = [
+
+['Glauco', 'José', 'Eugenio', 'Tu'],
+
+['Gabriela', 'Edilene', 'Laila', 'Terezinha'],
+
+['Rajadinha', 'Tereza', 'Limão'],
+
+['Tetê', 'Chico', 'Liz', 'Marley', 'Salusa']
+
+]
+
+print(salas[1][3])
