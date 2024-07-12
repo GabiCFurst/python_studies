471942c4ea957d1f074fcedf78b19e4744794a84
Gabriela Furst <gabifurst2@gmail.com>
2024-07-12 18:47:02 -0300

    Introdução à definição de fuções

---
+++ int_def_funcao.py
@@ -0,0 +1,15 @@
+def Print():
+    print('Eugenio')
+    print('Laila')
+    print('Edilene')
+    print('Gabi')
+    print('Glauco')
+    print('Tu')
+    
+
+
+Print()
+Print()
+Print()
+Print()
+Print()
