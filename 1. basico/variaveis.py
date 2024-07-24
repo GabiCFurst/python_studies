4e21d75bf3bab1e9dfa73b0d44b8bdc143396583
Gabriela Furst <gabifurst2@gmail.com>
2024-07-24 16:59:20 -0300

    Nova pasta!

---
+++ 1. basico/variaveis.py
@@ -0,0 +1,15 @@
+nome = 'Gabi'
+sobrenome = 'Figueira'
+ano_de_nascimento = 1967
+altura = 1.80
+peso = 50.5
+idade = 2024 - ano_de_nascimento
+maior_de_idade = idade >= 18
+
+print(f'Nome: {nome}')
+print(f'Sobrenome: {sobrenome}')
+print(f'Ano que nasceu: {ano_de_nascimento}')
+print(f'Altura: {altura}')
+print(f'Peso: {peso}')
+print(f'Idade: {idade}')
+print(f'{nome} é maior de idade? {maior_de_idade}')
